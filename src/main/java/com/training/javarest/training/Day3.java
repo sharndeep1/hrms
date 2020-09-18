b080bd8c1a10f3a9020443a1ee52716dc01f183e
sharndeep1 <sharndeep.chahal@yahoo.in>
2020-09-18 12:54:47 -0500

    added day3

--- src/main/java/com/training/javarest/training/Day3.java
+++ src/main/java/com/training/javarest/training/Day3.java
@@ -3,5 +3,5 @@ package com.training.javarest.training;
 public class Day3 {
 
     // git status -- check how many local files changed
-    // git add .  -- add all files to repository
+    // git add .  -- all files will be track for commit
 }
